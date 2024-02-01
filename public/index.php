f300123b620301b62a36a70f492297d6427d22b4
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-01 14:36:32 +0100

    add code

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
+<?php
+require_once "../bootstrap/app.php";
+require_once "../route/web.php";
 
+$filterURI;
+
+ob_start();
+if (!isset($_GET["action"])) {
+    header("Location: index.php?action=home");
+} else {
+    require_once "../ressources/views/layouts/main.tpl.php";
+}
+$content = ob_get_clean();
+echo $content;
