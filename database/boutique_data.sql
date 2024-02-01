70d0c479dae1516413d061982f1c540c2ca07522
dumasg <geremydumas+github@gmail.com>
2024-02-01 16:13:27 +0100

    ajout d'une query simple pour la génération d'un première article

---
+++ database/boutique_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (name, description, stock, path_img, price_ht, price_ttc, price_tva, weight, categories_id, tva_id)
+VALUES
+    ("Axis", "La suite de spin", 2, "eee", 20, 24, 4, 0.6, 1, 1);
