f6caa664413b89d283ea72a659ad4423260e20f2
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-01 15:30:56 +0100

    add HTML code in show.tpl.php

---
+++ ressources/views/product/show.tpl.php
@@ -0,0 +1,19 @@
+<div class="show__hero">
+    <h1>Ma boutique</h1>
+    <img src="" alt="" />
+</div>
+<div class="show__product">
+    <h2>Le produit</h2>
+    <div class="show__product_content">
+        <img src="" alt="" />
+        <div class="show__product_description">
+            <p><?= "DESC." ?></p>
+            <div class="show__product_price">
+                <p><?= "PRICE" ?> TTC</p>
+                <form action="index.php?action=cart" method="post">
+                    <input type="submit" value="ADD CART" />
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
