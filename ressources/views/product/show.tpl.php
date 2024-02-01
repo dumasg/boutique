c6af74ee20f46104f5a1d0fdd2905c67e62c3f62
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-01 16:36:04 +0100

    add content in show.tpl.php

--- ressources/views/product/show.tpl.php
+++ ressources/views/product/show.tpl.php
@@ -1,15 +1,17 @@
 <div class="show__hero">
     <h1>Ma boutique</h1>
-    <img src="" alt="" />
+    <img src="../img/andras-vas-Bd7gNnWJBkU-unsplash.jpg" alt="Laptop" />
 </div>
 <div class="show__product">
     <h2>Le produit</h2>
     <div class="show__product_content">
-        <img src="" alt="" />
+        <div class="show__product_picture">
+            <img src="../img/andras-vas-Bd7gNnWJBkU-unsplash.jpg" alt="Laptop" />
+        </div>
         <div class="show__product_description">
-            <p><?= "DESC." ?></p>
+            <p><?= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in felis et est tincidunt egestas sit amet ac elit. Pellentesque lobortis convallis mi id vestibulum. Mauris at ligula vitae nunc commodo malesuada eu et massa. Donec interdum in eros eu tincidunt. Vestibulum et nisi laoreet, imperdiet ipsum lacinia, vulputate lorem. Proin nisl enim, tincidunt vel pellentesque et, interdum a augue. Suspendisse dolor nibh, dictum nec scelerisque et, facilisis in risus. Morbi pulvinar, est duis." ?></p>
             <div class="show__product_price">
-                <p><?= "PRICE" ?> TTC</p>
+                <p><?= "18.50" ?> € TTC</p>
                 <form action="index.php?action=cart" method="post">
                     <input type="submit" value="ADD CART" />
                 </form>
