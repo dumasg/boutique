001a1c0ca9fc685607f2f9cd42d3de4e14a76b90
dumasg <geremydumas+github@gmail.com>
2024-02-01 16:47:16 +0100

    travail sur l'affichage de la home page

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,4 +1,20 @@
-<?php
-?>
+<div class="container">
+    <div class="container_articles">
+    <?php foreach ($articles as $key => $value) { ?>
+            <div class="card border">
+                <div class="card_img">
+                    <img src="<?= $value['path_img'] ?>" alt="">
+                </div>
+                <div class="card_body">
+                    <h3><?= $value['name'] ?></h3>
+                    <p><?= $value['description'] ?></p>
+                    <div class="call_to_action_product">
+                        <button class="btn btn-primary">Ajouter au panier</button>
+                        <p><?= $value['price_ttc'] ?> €</p>
+                    </div>
+                </div>
+            </div>
+    <?php } ?>
+    </div>
+</div>
 
-<h1>HOME TPL</h1>
