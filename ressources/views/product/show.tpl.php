fbfff5088437a3922b81af19b28fd7adde9e0f2c
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-02 09:52:06 +0100

    update show page

--- ressources/views/product/show.tpl.php
+++ ressources/views/product/show.tpl.php
@@ -3,19 +3,24 @@
     <img src="../img/andras-vas-Bd7gNnWJBkU-unsplash.jpg" alt="Laptop" />
 </div>
 <div class="show__product">
-    <h2>Le produit</h2>
-    <div class="show__product_content">
-        <div class="show__product_picture">
-            <img src="../img/andras-vas-Bd7gNnWJBkU-unsplash.jpg" alt="Laptop" />
-        </div>
-        <div class="show__product_description">
-            <p><?= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in felis et est tincidunt egestas sit amet ac elit. Pellentesque lobortis convallis mi id vestibulum. Mauris at ligula vitae nunc commodo malesuada eu et massa. Donec interdum in eros eu tincidunt. Vestibulum et nisi laoreet, imperdiet ipsum lacinia, vulputate lorem. Proin nisl enim, tincidunt vel pellentesque et, interdum a augue. Suspendisse dolor nibh, dictum nec scelerisque et, facilisis in risus. Morbi pulvinar, est duis." ?></p>
-            <div class="show__product_price">
-                <p><?= "18.50" ?> € TTC</p>
-                <form action="index.php?action=cart" method="post">
-                    <input type="submit" value="ADD CART" />
-                </form>
+    <?php for ($i = 0; $i < count($products); $i++) :
+        if ($_GET["id"] == $products[$i]["id"]) : ?>
+            <h2><?= $products[$i]["name"] ?></h2>
+            <div class="show__product_content">
+                <div class="show__product_picture">
+                    <img src="<?= $products[$i]["path_img"] ?>" alt="Laptop" />
+                </div>
+                <div class="show__product_description">
+                    <p><?= $products[$i]["description"] ?></p>
+                    <div class="show__product_price">
+                        <p><?= $products[$i]["price_ttc"] ?> € TTC</p>
+                        <form action="index.php?action=cart" method="post">
+                            <input type="number" id="quantity" name="quantity" min="1" value="1" />
+                            <input type="submit" value="ADD CART" />
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+    <?php endif;
+    endfor; ?>
 </div>
