3bf5209ed4b4921ae14f2cff8e7c42e73a75a441
dumasg <geremydumas+github@gmail.com>
2024-02-02 09:51:12 +0100

    mise en place du css de base ainsi que de la homepage

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,10 +1,13 @@
 <div class="container">
+    <h1 class="title_page">Nos derniers ajouts </h1>
     <div class="container_articles">
-    <?php foreach ($articles as $key => $value) { ?>
+        <?php foreach ($articles as $key => $value) { ?>
             <div class="card border">
-                <div class="card_img">
-                    <img src="<?= $value['path_img'] ?>" alt="">
-                </div>
+                <a href="">
+                    <div class="card_img">
+                        <img src="<?= $value['path_img'] ?>" alt="">
+                    </div>
+                </a>
                 <div class="card_body">
                     <h3><?= $value['name'] ?></h3>
                     <p><?= $value['description'] ?></p>
@@ -14,7 +17,7 @@
                     </div>
                 </div>
             </div>
-    <?php } ?>
+        <?php } ?>
     </div>
 </div>
 
