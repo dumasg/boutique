17698ab1c3eb3dba2a4c9e5d33e2d0c787880cf7
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-02 11:51:16 +0100

    update index - add redirect link to home

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ require_once "../route/web.php";
 $filterURI = filter_input(INPUT_GET, "action", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 ob_start();
-if (!isset($_GET["action"])) {
-    header("Location: index.php?action=product&id=1");
+if (!isset($_GET["action"]) && empty($paths[$filterURI])) {
+    require_once "../ressources/views/layouts/main.tpl.php";
 } else {
     require_once "../ressources/views/layouts/main.tpl.php";
 }
