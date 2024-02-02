8910041b6fcb26debbea3df7606bcc141d4c9034
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-02 16:47:46 +0100

    add path to cart page

--- route/web.php
+++ route/web.php
@@ -2,4 +2,5 @@
 $paths = [
     "" => "../app/controllers/homeController.php",
     "product" => "../app/controllers/productController.php",
+    "cart" => "../app/controllers/cartController.php",
 ];
