4b356d36e356ff06ae6de65f193ae966f8dcd846
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-02 11:51:57 +0100

    add getAllProducts function

--- app/persistences/productData.php
+++ app/persistences/productData.php
@@ -1,7 +1,21 @@
 <?php
-function getProduct($db) {
+function getAllProducts($db) {
     $query = "SELECT *
     FROM products";
+    
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
+}
+
+function getProduct($db, $product_id) {
+    $query = "SELECT *
+    FROM products 
+    WHERE products.id = '$product_id'";
     
     $arr = array();
 
