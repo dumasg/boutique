ec20a50fe2ebb4f685e4811e2b0b57b26685ebac
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-02-06 10:32:58 +0100

    add session for cart and badgeCart, and recover product

--- app/controllers/cartController.php
+++ app/controllers/cartController.php
@@ -1,11 +1,14 @@
 <?php
+require_once "../app/persistences/productData.php";
 require_once "../app/persistences/cart.php";
 
 try {
-    $products = fakeCart($db);
-    $_SESSION["cart"] = totalCart($products);
+    initCart();
+    $_SESSION["cart"] = fakeCart($db, 1);
+    $_SESSION["totalCart"] = totalCart($db, $_SESSION["cart"]);
+    $productsCart = getAllProducts($db);
 } catch (Exception $e) {
     echo 'Exception reçue : ',  $e->getMessage(), "\n";
 }
 
-require_once "../ressources/views/cart/index.tpl.php";
+require_once "../ressources/views/cart/cart.tpl.php";
